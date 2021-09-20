c1a5087c702a8f8fbf063fd83707638870460ffa
MichailParaskevopoulos <michail@badal.io>
2021-09-20 15:39:04 +0000

    Fixed typo

--- src/airflow_framework/base_class/table_config.py
+++ src/airflow_framework/base_class/table_config.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> AF-14-API-plugin-Twilio-POC
-=======
->>>>>>> Merge conflict resolved
-=======
->>>>>>> c1bd6b76fc88f70ee3ab287d529c04246fa36bc2
 from airflow.exceptions import AirflowException
 
 from dacite import Config
