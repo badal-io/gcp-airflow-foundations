53386955756fda8d054b404cf21abc3ff82e2c2d
danielkulikov <daniel.kulikov@gmail.com>
2021-10-19 14:43:22 -0400

    added init,enum for salesforce source

--- src/gcp_airflow_foundations/source_class/__init__.py
+++ src/gcp_airflow_foundations/source_class/__init__.py
@@ -2,7 +2,7 @@ from gcp_airflow_foundations.enums.source_type import SourceType
 
 from gcp_airflow_foundations.source_class.gcs_source import GCStoBQDagBuilder
 from gcp_airflow_foundations.source_class.twilio_source import TwilioToBQDagBuilder
-
+from gcp_airflow_foundations.source_class.salesforce_source import SalesforcetoBQDagBuilder
 from gcp_airflow_foundations.base_class.data_source_table_config import DataSourceTablesConfig
 
 import logging
@@ -16,6 +16,9 @@ def get_dag_builder(source: SourceType, default_task_args: dict, config: DataSou
     elif source == SourceType.TWILIO:
         logging.info("Selecting Twilio builder")
         return TwilioToBQDagBuilder(default_task_args, config)
+    elif source == SourceType.SALESFORCE:
+        logging.info("Selecting Salesforce builder")
+        return SalesforcetoBQDagBuilder(default_task_args, config)
     else:
         logging.info(f"Invalid source {source}")
         raise Exception(f"Invalid source {source}")
