59cfdaa29bb4222b54add8d9e19a10d1c2d8059c
Jin Song <jinysongtoronto@gmail.com>
2021-10-19 00:55:08 -0400

    -added bq_hook.create_empty_table -cleaned up execute to 3 functions -removed print and used logging and AirflowException -moved BigQueryHook and its cursor to the constructor class to removed redundancy -!!! issue: changed execute to run_query with write_disposition="write_truncate", however table only gets appended, not truncated

--- src/airflow_framework/plugins/api/operators/twilio_validate_operator.py
+++ src/airflow_framework/plugins/api/operators/twilio_validate_operator.py
@@ -1,24 +1,22 @@
 from airflow.models.baseoperator import BaseOperator
-from airflow.utils.decorators import apply_defaults
 from airflow.hooks.base import BaseHook
-from airflow.providers.google.cloud.hooks.bigquery import (
-    BigQueryBaseCursor,
-    BigQueryConnection,
-    BigQueryCursor,
-    BigQueryHook
-)
-from typing import Any, Callable, Dict, Optional, Type
-from twilio.rest import Client
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 
+from airflow.utils.decorators import apply_defaults
+from airflow.exceptions import AirflowException
+
+from twilio.rest import Client
+from typing import Any
+import logging
 
 
 class ValidateNumberOperator(BaseOperator):
     """
         Gets a column of phone numbers from a Bigquery table,
         validates these phone numbers using the Twilio Validate API,
-        and merges result of phone number validation back into the source table as a column 'is_valid' (boolean).
+        and merges result of the validated phone number back into the source table as a column 'is_valid' (boolean).
 
-        connections requirement: twilio & google
+        connections require: twilio & GCP
 
         Attributes:
         bigquery_conn_id
@@ -30,110 +28,108 @@ class ValidateNumberOperator(BaseOperator):
         phone_number_column_name: column name of source table containing phone numbers for validation (string)
         """
 
-    twilioClient = None
-
     @apply_defaults
     def __init__(self, *, bigquery_conn_id: str, twilio_conn_id: str, project: str, dataset: str, temp_table: str, source_table: str, phone_number_column_name: str, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        self.bigquery_conn_id = bigquery_conn_id
-        self.twilio_conn_id = twilio_conn_id
+        self.bq_hook = self.get_bq_hook(bigquery_conn_id)
+        self.cursor = self.bq_hook.get_conn().cursor()
+
+        self.twilio_client = self.get_twilio_client(twilio_conn_id)
+
         self.project = project
         self.dataset = dataset
-        self.temp_table = temp_table
         self.source_table = source_table
         self.phone_number_column_name = phone_number_column_name
+        self.temp_table = 'temp_table_for_twilio_phone_number_validation'
 
-        # check if twilioClient is available before connecting
-        if ValidateNumberOperator.twilioClient is None:
-            try:
-                connection = BaseHook.get_connection(self.twilio_conn_id)
-                account_sid = connection.login
-                auth_token = connection.password
-                ValidateNumberOperator.twilioClient = Client(account_sid, auth_token)
-            except Exception as error:
-                print("Error: Connection not established {}".format(error))
-            else:
-                print("Connection established")
-        self.twilio_client = ValidateNumberOperator.twilioClient
+
 
     def execute(self, context):
-        # 1. get list of phone numbers from BigQuery column
-        number_list = self.get_from_bq()
 
-        # 2. validate phone numbers with Twilio Validate API
+        # 1. create temp table in BigQuery
+        self.create_temp_table()
+
+        # 2. update BigQuery with valid phone numbers from Twilio
+        self.update_validation()
+
+        # 3. add column 'is_valid' to source table in BigQuery
+        self.add_column()
+
+    def get_bq_hook(self, bq_conn_id):
+        try:
+            hook = BigQueryHook(bq_conn_id, use_legacy_sql=False)
+            logging.info("BigQuery connection established")
+            return hook
+        except Exception as e:
+            raise AirflowException(f"Error BigQuery connection not established: {e}")
+
+    def get_twilio_client(self, conn_id):
+        try:
+            connection = BaseHook.get_connection(conn_id)
+            account_sid = connection.login
+            auth_token = connection.password
+            twilioClient = Client(account_sid, auth_token)
+            logging.info("Twilio Connection established")
+            return twilioClient
+        except Exception as e:
+            raise AirflowException(f"Error Twilio connection not established: {e}")
+
+    def create_temp_table(self):
+        self.bq_hook.create_empty_table(
+            project_id=self.project,
+            dataset_id=self.dataset,
+            table_id=self.temp_table,
+            # exist_ok=True,
+            schema_fields=[
+                {'name':'phone_number', 'type':'STRING'},
+                {'name':'is_valid', 'type':'BOOL'}
+            ]
+        )
+
+    def update_validation(self):
+        # get list of phone numbers from source table in BigQuery
+        self.cursor.execute(
+            f'''SELECT {self.phone_number_column_name} FROM {self.project}.{self.dataset}.{self.source_table}''')
+        result = self.cursor.fetchall()
+        fetched_numbers = []
+        for x in result:
+            fetched_numbers.append(x[0])
+
+        # validate list of phone numbers with Twilio API
         valid_number_list = []
         invalid_number_list =[]
 
-        for number in number_list:
+        for number in fetched_numbers:
             is_valid = self.process_number(number)
             if is_valid:
                 valid_number_list.append(number)
             if not is_valid:
                 invalid_number_list.append(number)
-        print("number of invalid numbers ------------>", len(invalid_number_list))
-        print("number of invalid numbers ------------>", len(valid_number_list))
 
-        # 3. update temp table in bq
-        self.update_bq(valid_number_list, invalid_number_list)
+        # write validated number to temp table in BigQuery
+        sql_str = ''
+        for x in valid_number_list:
+            sql_str += f'("{x}", True),\n'
+        for x in invalid_number_list:
+            sql_str += f'("{x}", False),\n'
+        sql_str = 'VALUES \n' + sql_str[0:-2]
 
-        # 4. add is_valid column to source table
-        self.add_column()
-
-        # 5. truncate temporary table
-        self.truncate_temp_table() # works but no point if you have to add the temp table automatically
-
-    def get_from_bq(self):
-        hook = BigQueryHook(bigquery_conn_id=self.bigquery_conn_id, use_legacy_sql=False)
-        conn = hook.get_conn()
-        cursor = conn.cursor()
-        cursor.execute(f'''SELECT {self.phone_number_column_name} FROM {self.project}.{self.dataset}.{self.source_table}''')
-        result = cursor.fetchall()
-
-        fetched_numbers = []
-        for x in result:
-            fetched_numbers.append(x[0])
-
-        return fetched_numbers
+        self.bq_hook.run_query(sql=f'''INSERT INTO {self.project}.{self.dataset}.{self.temp_table}
+                                    (phone_number, is_valid)
+                                    {sql_str}''',
+                               write_disposition='WRITE_TRUNCATE',
+                               use_legacy_sql=False)
 
     def process_number(self, number):
         try:
-            phone_number = self.twilio_client.lookups.v1.phone_numbers(number).fetch(country_code='US')
-            print(phone_number.phone_number)
+            self.twilio_client.lookups.v1.phone_numbers(number).fetch(country_code='US')
             return True
         except:
             return False
 
-    def update_bq(self, valid_numbers, invalid_numbers):
-
-        sql_str = ''
-        for x in valid_numbers:
-            sql_str += f'("{x}", True),\n'
-        for x in invalid_numbers:
-            sql_str += f'("{x}", False),\n'
-        sql_str = 'VALUES \n' + sql_str[0:-2]
-
-        hook = BigQueryHook(bigquery_conn_id=self.bigquery_conn_id, use_legacy_sql=False)
-        conn = hook.get_conn()
-        cursor = conn.cursor()
-
-        cursor.execute(f'''INSERT INTO {self.project}.{self.dataset}.{self.temp_table} 
-                            (phone_number, is_valid)
-                            {sql_str}''')
-
     def add_column(self):
-        hook = BigQueryHook(bigquery_conn_id=self.bigquery_conn_id, use_legacy_sql=False)
-        conn = hook.get_conn()
-        cursor = conn.cursor()
-
-        cursor.execute(f'''MERGE `{self.project}.{self.dataset}.{self.source_table}` AS TARGET
+        self.cursor.execute(f'''MERGE `{self.project}.{self.dataset}.{self.source_table}` AS TARGET
                             USING `{self.project}.{self.dataset}.{self.temp_table}` AS SOURCE
-                            ON (TARGET.phone_number = SOURCE.{self.phone_number_column_name})
+                            ON (TARGET.{self.phone_number_column_name} = SOURCE.phone_number)
                             WHEN MATCHED THEN UPDATE
                             SET TARGET.is_valid = SOURCE.is_valid''')
-
-    def truncate_temp_table(self):
-        hook = BigQueryHook(bigquery_conn_id=self.bigquery_conn_id, use_legacy_sql=False)
-        conn = hook.get_conn()
-        cursor = conn.cursor()
-
-        cursor.execute(f'''TRUNCATE TABLE `{self.project}.{self.dataset}.{self.temp_table}`''')
