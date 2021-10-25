7e5c8485fb680ac2bd8e7fa42e2cc811a0b63107
Jin Song <jinysongtoronto@gmail.com>
2021-10-25 02:57:57 -0400

    mailgun email validation: hook, custom operators, sensor, and dag

--- src/airflow_framework/plugins/api/hooks/mailgun_hook_test.py
+++ src/airflow_framework/plugins/api/hooks/mailgun_hook_test.py
@@ -1,5 +1,7 @@
 import requests
 from airflow.hooks.base import BaseHook
+import logging
+
 
 class MailgunHook(BaseHook):
     """
@@ -14,6 +16,7 @@ class MailgunHook(BaseHook):
         'risk': 'low'
     }
     """
+
     def __init__(self, mailgun_conn_id: str) -> dict:
         self.conn = self.get_connection(mailgun_conn_id)
         self.session = requests.Session()
@@ -25,3 +28,20 @@ class MailgunHook(BaseHook):
             params={'address': email}
         )
         return res.json()
+
+    def validate_email_bulk(self, file, job_id):
+        res = self.session.post(
+            f'https://api.mailgun.net/v4/address/validate/bulk/{job_id}',
+            files={'file': open(file, 'rb')}
+        )
+        return res.json()
+
+    def check_job_status(self, job_id):
+        res = self.session.get(f'https://api.mailgun.net/v4/address/validate/bulk/{job_id}')
+        logging.info(f'job status: {res.json()}')
+        return res
+
+    def download_csv(self, job_id):
+        res_job = self.session.get(f'https://api.mailgun.net/v4/address/validate/bulk/{job_id}')
+        csv_link = res_job.json()['download_url']['csv']
+        return csv_link
