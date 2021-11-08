75358d15b7113effdaa24b700f69be0db094bfb5
danielkulikov <daniel.kulikov@gmail.com>
2021-11-08 11:00:18 -0500

    merge conflicts

--- src/gcp_airflow_foundations/enums/source_type.py
+++ src/gcp_airflow_foundations/enums/source_type.py
@@ -6,9 +6,5 @@ class SourceType(Enum):
     GCS = "GCS"
     TWILIO = "TWILIO"
     BQ = "BQ"
-<<<<<<< HEAD
     SALESFORCE = "SALESFORCE"
     ORACLE = "ORACLE"
-=======
-    SALESFORCE = "SALESFORCE"
->>>>>>> 07e642acb5747f577c9dda5f86471ba95b72e454
