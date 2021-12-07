1561fd7fd6553ac24382dcdbdd361fa44574a5dd
MichailParaskevopoulos <paraskevopoulos.michail@gmail.com>
2021-12-07 17:57:16 +0000

    Directory check

--- helpers/scripts/airflow-ci.sh
+++ helpers/scripts/airflow-ci.sh
@@ -3,6 +3,6 @@ pip install -U pip
 
 PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 
-CONSTRAINT_URL=https://raw.githubusercontent.com/apache/airflow/constraints-$1/constraints-${PYTHON_VERSION}.txt
+#CONSTRAINT_URL=https://raw.githubusercontent.com/apache/airflow/constraints-$1/constraints-${PYTHON_VERSION}.txt
 
-pip install apache-airflow[gcp_api]==$1 --constraint ${CONSTRAINT_URL}
+#pip install apache-airflow[gcp_api]==$1 --constraint ${CONSTRAINT_URL}
