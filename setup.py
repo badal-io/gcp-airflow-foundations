05e877bff04aa90fca3f794709323f22663688b0
MichailParaskevopoulos <paraskevopoulos.michail@gmail.com>
2021-12-13 20:26:31 +0000

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
+import setuptools
 from setuptools import setup, find_packages
+
 from pathlib import Path
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -13,22 +15,29 @@ with open(os.path.join(here, "requirements.txt"), "r") as f:
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-TAG = "v{}.{}.{}".format(0, 2, 7)
+packages = [
+    package
+    for package in setuptools.PEP420PackageFinder.find()
+    if package.startswith("gcp_airflow_foundations")
+]
+
+version = {}
+with open(os.path.join(here, "gcp_airflow_foundations/version.py")) as fp:
+    exec(fp.read(), version)
+version = version["__version__"]
 
 def main():
     metadata = dict(
         name="gcp-airflow-foundations",
-        version="0.2.7",
+        version=version,
         description="Opinionated framework based on Airflow 2.0 for building pipelines to ingest data into a BigQuery data warehouse",
         long_description=long_description,
         long_description_content_type='text/markdown',
         url="https://github.com/badal-io/gcp-airflow-foundations",
-        download_url=f"https://github.com/badal-io/gcp-airflow-foundations/archive/refs/tags/{TAG}.tar.gz",
         author="Badal.io",
         author_email="info@badal.io",
         license="Apache 2.0",
-        packages=find_packages(where="gcp_airflow_foundations", exclude=("tests")),
-        package_dir={"":"gcp_airflow_foundations"},
+        packages=packages,
         install_requires=requirements,
         classifiers=[
             'Development Status :: 4 - Beta',      
