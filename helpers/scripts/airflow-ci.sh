5057b8117228c73b68dff9a242ddf088fa15bfa9
MichailParaskevopoulos <paraskevopoulos.michail@gmail.com>
2021-12-17 17:11:49 +0000

    Dockerfile

---
+++ helpers/scripts/airflow-ci.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip install -U pip
+
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -17,6 +17,7 @@ import logging
 
 def run_task(task, execution_date):
     ti = TaskInstance(task=task, execution_date=execution_date)
+    task.run(start_date=execution_date, end_date=execution_date, ignore_ti_state=True)
     task.execute(ti.get_template_context())
 
 @pytest.fixture(scope="session")
