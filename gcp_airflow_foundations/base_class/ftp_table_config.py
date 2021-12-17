78b33170f6673ed0b26b853de2303d76c460b360
danielkulikov <daniel.kulikov@gmail.com>
2021-12-17 01:39:21 -0500

    refactored ftp configs

---
+++ gcp_airflow_foundations/base_class/ftp_table_config.py
@@ -0,0 +1,19 @@
+from pydantic import validator
+from pydantic.dataclasses import dataclass
+
+from typing import List, Optional
+
+@dataclass
+class FTPTableConfig:
+    """
+    Attributes:
+        directory_prefix: optional directory prefix override per table
+        metadata_file: the relative path to the metadata file within the GSC bucket
+        schema_file: the (optional) relative path to the schema file for the table
+        source_format: file format for all files in this metadata file
+        date_table: optional date column name for external partitions for SFTP directory transfer
+    """
+    directory_prefix: Optional[str]
+    metadata_file: Optional[str]
+    schema_file: Optional[str]
+    flag_file_path: Optional[str]
