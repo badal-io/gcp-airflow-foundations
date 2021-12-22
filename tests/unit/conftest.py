9ec076db33558dcca43d6fce6f355e93d8e5aa6a
MichailParaskevopoulos <paraskevopoulos.michail@gmail.com>
2021-12-22 22:00:16 +0000

    Removed fixture

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -21,13 +21,3 @@ DEFAULT_DATE = datetime(2015, 1, 1)
 def execute_task(task, execution_date):
     ti = TaskInstance(task=task, execution_date=execution_date)
     task.execute(ti.get_template_context())
-
-@pytest.fixture(autouse=True)
-def create_dummy_task_instance():
-    args = {'owner': 'airflow', 'start_date': DEFAULT_DATE}
-    
-    dag = DAG('TEST_DAG_ID', default_args=args, schedule_interval='@once')
-
-    task = DummyOperator(task_id='dummy', dag=dag)
-
-    return TaskInstance(task=task, execution_date=DEFAULT_DATE)
