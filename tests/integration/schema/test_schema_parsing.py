4817660290a31633cac743b07bcebb5092531770
Eugene Miretsky <eugene@badal.io>
2022-02-02 10:55:08 -0500

    test

--- tests/integration/schema/test_schema_parsing.py
+++ tests/integration/schema/test_schema_parsing.py
@@ -1,98 +1,96 @@
-import unittest
-from unittest import mock
-from unittest.mock import MagicMock
 import os
-
-import pytest
-from google.cloud.exceptions import Conflict
-
-from datetime import datetime
 import pytz
-
+import unittest
+from airflow.models import DAG, TaskInstance, XCom, DagRun, DagTag, DagModel
 from airflow.operators.dummy import DummyOperator
-from airflow.exceptions import AirflowException
-from airflow.models import (
-    DAG,
-    TaskInstance,
-    XCom,
-    DagBag, 
-    DagRun, 
-    DagTag,
-    DagModel
-)
-from airflow.models.xcom import XCOM_RETURN_KEY
+from datetime import datetime
 
-from gcp_airflow_foundations.operators.gcp.schema_parsing.schema_parsing_operator import ParseSchema
-from gcp_airflow_foundations.parse_dags import DagParser
 from gcp_airflow_foundations.base_class.utils import load_tables_config_from_dir
-from gcp_airflow_foundations.source_class.schema_source_config import AutoSchemaSourceConfig, GCSSchemaSourceConfig, BQLandingZoneSchemaSourceConfig
-
-TASK_ID = 'test-bq-generic-operator'
-DEFAULT_DATE = pytz.utc.localize(datetime(2017, 8, 1))
-TEST_DAG_ID = 'test-bigquery-operators'
-
+from gcp_airflow_foundations.operators.gcp.schema_parsing.schema_parsing_operator import (
+    ParseSchema,
+)
+from gcp_airflow_foundations.source_class.schema_source_config import (
+    BQLandingZoneSchemaSourceConfig,
+)
 from airflow.utils.session import create_session, provide_session
 from airflow.utils.state import State
-from airflow.utils import timezone
+
+TASK_ID = "test-bq-generic-operator"
+DEFAULT_DATE = pytz.utc.localize(datetime(2017, 8, 1))
+TEST_DAG_ID = "test-bigquery-operators"
+
+
 
 expected_xcom = {
-    'source_table_columns': [
-        'visitorId', 
-        'visitNumber', 
-        'visitId', 
-        'visitStartTime', 
-        'date', 
-        'fullVisitorId', 
-        'userId', 
-        'clientId', 
-        'channelGrouping', 
-        'socialEngagementType', 
-        'customDimensions'
+    "source_table_columns": [
+        "visitorId",
+        "visitNumber",
+        "visitId",
+        "visitStartTime",
+        "date",
+        "fullVisitorId",
+        "userId",
+        "clientId",
+        "channelGrouping",
+        "socialEngagementType",
+        "customDimensions",
     ],
-    'af_test_ods.ga_sessions_ODS': [
-        {'name': 'visitorId', 'type': 'INTEGER', 'mode': 'NULLABLE'},
-        {'name': 'visitNumber', 'type': 'INTEGER', 'mode': 'NULLABLE'}, 
-        {'name': 'visitId', 'type': 'INTEGER', 'mode': 'NULLABLE'}, 
-        {'name': 'visitStartTime', 'type': 'INTEGER', 'mode': 'NULLABLE'}, 
-        {'name': 'date', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'fullVisitorId', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'userId', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'clientId', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'channelGrouping', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'socialEngagementType', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'customDimensions', 'type': 'RECORD', 'mode': 'REPEATED', 'fields': [
-            {'name': 'index', 'type': 'INTEGER', 'mode': 'NULLABLE'}, 
-            {'name': 'value', 'type': 'STRING', 'mode': 'NULLABLE'}
-        ]}, 
-        {'name': 'af_metadata_inserted_at', 'type': 'TIMESTAMP'}, 
-        {'name': 'af_metadata_updated_at', 'type': 'TIMESTAMP'}, 
-        {'name': 'af_metadata_primary_key_hash', 'type': 'STRING'}, 
-        {'name': 'af_metadata_row_hash', 'type': 'STRING'}
-    ], 
-    'af_test_hds.ga_sessions_HDS': [
-        {'name': 'visitorId', 'type': 'INTEGER', 'mode': 'NULLABLE'}, 
-        {'name': 'visitNumber', 'type': 'INTEGER', 'mode': 'NULLABLE'}, 
-        {'name': 'visitId', 'type': 'INTEGER', 'mode': 'NULLABLE'}, 
-        {'name': 'visitStartTime', 'type': 'INTEGER', 'mode': 'NULLABLE'}, 
-        {'name': 'date', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'fullVisitorId', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'userId', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'clientId', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'channelGrouping', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'socialEngagementType', 'type': 'STRING', 'mode': 'NULLABLE'}, 
-        {'name': 'customDimensions', 'type': 'RECORD', 'mode': 'REPEATED', 'fields': [
-            {'name': 'index', 'type': 'INTEGER', 'mode': 'NULLABLE'}, 
-            {'name': 'value', 'type': 'STRING', 'mode': 'NULLABLE'}
-        ]}, 
-        {'name': 'af_metadata_created_at', 'type': 'TIMESTAMP'}, 
-        {'name': 'af_metadata_expired_at', 'type': 'TIMESTAMP'}, 
-        {'name': 'af_metadata_row_hash', 'type': 'STRING'}
-    ]
+    "af_test_ods.ga_sessions_ODS": [
+        {"name": "visitorId", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "visitNumber", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "visitId", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "visitStartTime", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "date", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "fullVisitorId", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "userId", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "clientId", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "channelGrouping", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "socialEngagementType", "type": "STRING", "mode": "NULLABLE"},
+        {
+            "name": "customDimensions",
+            "type": "RECORD",
+            "mode": "REPEATED",
+            "fields": [
+                {"name": "index", "type": "INTEGER", "mode": "NULLABLE"},
+                {"name": "value", "type": "STRING", "mode": "NULLABLE"},
+            ],
+        },
+        {"name": "af_metadata_inserted_at", "type": "TIMESTAMP"},
+        {"name": "af_metadata_updated_at", "type": "TIMESTAMP"},
+        {"name": "af_metadata_primary_key_hash", "type": "STRING"},
+        {"name": "af_metadata_row_hash", "type": "STRING"},
+    ],
+    "af_test_hds.ga_sessions_HDS": [
+        {"name": "visitorId", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "visitNumber", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "visitId", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "visitStartTime", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "date", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "fullVisitorId", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "userId", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "clientId", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "channelGrouping", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "socialEngagementType", "type": "STRING", "mode": "NULLABLE"},
+        {
+            "name": "customDimensions",
+            "type": "RECORD",
+            "mode": "REPEATED",
+            "fields": [
+                {"name": "index", "type": "INTEGER", "mode": "NULLABLE"},
+                {"name": "value", "type": "STRING", "mode": "NULLABLE"},
+            ],
+        },
+        {"name": "af_metadata_created_at", "type": "TIMESTAMP"},
+        {"name": "af_metadata_expired_at", "type": "TIMESTAMP"},
+        {"name": "af_metadata_row_hash", "type": "STRING"},
+    ],
 }
+
 
 @provide_session
 def cleanup_xcom(session=None):
     session.query(XCom).delete()
+
 
 def clear_db_dags():
     with create_session() as session:
@@ -101,7 +99,7 @@ def clear_db_dags():
         session.query(DagRun).delete()
         session.query(TaskInstance).delete()
 
-    
+
 class TestParseSchema(unittest.TestCase):
     def setUp(self):
         here = os.path.abspath(os.path.dirname(__file__))
@@ -111,14 +109,17 @@ class TestParseSchema(unittest.TestCase):
 
         self.config = next((i for i in configs), None)
 
-        args = {'owner': 'airflow', 'start_date': DEFAULT_DATE}
-        self.dag = DAG('TEST_DAG_ID', default_args=args, schedule_interval='@once')
+        args = {"owner": "airflow", "start_date": DEFAULT_DATE}
+        self.dag = DAG("TEST_DAG_ID", default_args=args, schedule_interval="@once")
 
         self.dag.create_dagrun(
-            run_id='test', start_date=DEFAULT_DATE, execution_date=DEFAULT_DATE, state=State.SUCCESS
+            run_id="test",
+            start_date=DEFAULT_DATE,
+            execution_date=DEFAULT_DATE,
+            state=State.SUCCESS,
         )
 
-        task = DummyOperator(task_id='dummy', dag=self.dag)
+        task = DummyOperator(task_id="dummy", dag=self.dag)
         self.ti = TaskInstance(task=task, execution_date=DEFAULT_DATE)
 
         self.template_context = self.ti.get_template_context()
@@ -131,15 +132,15 @@ class TestParseSchema(unittest.TestCase):
         source_config = self.config.source
         table_config = next((i for i in self.config.tables), None)
 
-        table_config.ods_config.table_id = f'{table_config.table_name}_ODS'
-        table_config.hds_config.table_id = f'{table_config.table_name}_HDS'
+        table_config.ods_config.table_id = f"{table_config.table_name}_ODS"
+        table_config.hds_config.table_id = f"{table_config.table_name}_HDS"
 
         operator = ParseSchema(
-            task_id=TASK_ID, 
+            task_id=TASK_ID,
             schema_config=BQLandingZoneSchemaSourceConfig,
             column_mapping=None,
             data_source=source_config,
-            table_config=table_config
+            table_config=table_config,
         )
 
         return_value = operator.execute(context=self.template_context)
