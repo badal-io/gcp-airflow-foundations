fd04d37da02ea1249fdf5c49b5b38e8271541993
Eugene Miretsky <eugene@badal.io>
2022-02-02 19:44:00 -0500

    fix tests

--- tests/unit/utils/test_sql_helper_ods.py
+++ tests/unit/utils/test_sql_helper_ods.py
@@ -46,7 +46,7 @@ class TestSqlHelperODS(unittest.TestCase):
             == """
             MERGE `target_dataset.target` T
             USING `source_dataset.source` S
-            ON T.key=S.key
+            ON T.key=S.key 
             WHEN MATCHED THEN UPDATE
                 SET `key`=S.`key`,`column_b`=S.`column_a`,af_metadata_updated_at=CURRENT_TIMESTAMP(),af_metadata_row_hash=TO_BASE64(MD5(TO_JSON_STRING(S)))
             WHEN NOT MATCHED THEN
