2dc3f5b1f30776552f2bcde1ce1f6ac9d586a752
danielkulikov <daniel.kulikov@gmail.com>
2022-03-30 09:45:38 -0400

    fixed sftp sensor

--- gcp_airflow_foundations/operators/api/sensors/sftp_sensor.py
+++ gcp_airflow_foundations/operators/api/sensors/sftp_sensor.py
@@ -16,11 +16,12 @@
 # specific language governing permissions and limitations
 # under the License.
 from typing import Optional
-
+import logging
 from paramiko.sftp import SFTP_NO_SUCH_FILE
 
 from airflow.providers.sftp.hooks.sftp import SFTPHook
 from airflow.sensors.base import BaseSensorOperator
+from gcp_airflow_foundations.common.sftp.helpers import save_id_to_file
 
 
 class SFTPFilesExistenceSensor(BaseSensorOperator):
@@ -32,25 +33,30 @@ class SFTPFilesExistenceSensor(BaseSensorOperator):
     :type sftp_conn_id: str
     """
 
-    template_fields = ("paths",)
+    template_fields = ('paths',)
 
-    def __init__(
-        self, *, paths: list, sftp_conn_id: str = "sftp_default", **kwargs
-    ) -> None:
+    def __init__(self, *, dir_prefix: str, paths: list, key_auth = False, key_name = "", sftp_conn_id: str = 'sftp_default', **kwargs) -> None:
         super().__init__(**kwargs)
+        self.dir_prefix = dir_prefix
         self.paths = paths
+        self.key_auth = key_auth
+        self.key_name = key_name
         self.hook: Optional[SFTPHook] = None
         self.sftp_conn_id = sftp_conn_id
 
+        if key_auth:
+            save_id_to_file(self.key_name)
+
     def poke(self, context: dict) -> bool:
+        self.paths = [self.dir_prefix + "/" + p for p in self.paths]
         self.hook = SFTPHook(self.sftp_conn_id)
+        logging.info(self.paths)
         for path in self.paths:
-            self.log.info("Poking for %s", path)
+            logging.info(path)
+            self.log.info('Poking for %s', path)
             try:
                 mod_time = self.hook.get_mod_time(path)
-                self.log.info(
-                    "Found File %s last modified: %s", str(path), str(mod_time)
-                )
+                self.log.info('Found File %s last modified: %s', str(path), str(mod_time))
             except OSError as e:
                 if e.errno != SFTP_NO_SUCH_FILE:
                     raise e
