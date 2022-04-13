5c1dc21e705f0db52e11abefa5a0a443649ddc37
Jiny Song <jinysongtoronto@gmail.com>
2022-04-13 16:18:29 -0400

    Jiny song/dataform (#74)
    * dataform hook, operator, and sensor; cleanup example dag files
    * dataform operator and sensor: add submit mode option

---
+++ gcp_airflow_foundations/operators/api/sensors/dataform_sensor.py
@@ -0,0 +1,26 @@
+from gcp_airflow_foundations.operators.api.hooks.dataform_hook import DataformHook
+from airflow.sensors.base import BaseSensorOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.exceptions import AirflowException
+from typing import Any
+import logging
+
+class DataformSensor(BaseSensorOperator):
+    @apply_defaults
+    def __init__(self, task_ids: str, dataform_conn_id='dataform_conn_id', **kwargs: Any):
+        super().__init__(**kwargs)
+        self.task_ids = task_ids
+        self.hook = DataformHook(dataform_conn_id=dataform_conn_id)
+
+    def poke(self, context):
+        run_url = context['ti'].xcom_pull(key='return_value', task_ids=[self.task_ids])
+        response = self.hook.check_job_status(run_url[0])
+
+        if response == 'SUCCESSFUL':
+            logging.info(f"Dataform run completed: SUCCESSFUL see run logs at {run_url}")
+            return True
+        elif response == 'RUNNING':
+            logging.info(f"Dataform job running. {run_url}")
+            return False
+        else:
+            raise AirflowException(f"Dataform run {response}: see run logs at {run_url}")
