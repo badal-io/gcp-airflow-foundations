6de9b0344d42801373edb2f6582bcaf7172fd938
danielkulikov <daniel.kulikov@gmail.com>
2022-04-13 08:46:11 -0400

    missing files

---
+++ gcp_airflow_foundations/common/sftp/helpers.py
@@ -0,0 +1,7 @@
+from airflow.models import Variable
+
+def save_id_to_file(private_key_var):
+    key = Variable.get(private_key_var)
+    secret_path = "id_rsa"
+    with open(secret_path, "w") as f:
+        f.write(key)
