a76d8e6f916e0400d6f58017a9a7a41f421eebd3
danielkulikov <daniel.kulikov@gmail.com>
2022-04-28 16:28:52 -0400

    cleaned up source template config

--- gcp_airflow_foundations/base_class/source_template_config.py
+++ gcp_airflow_foundations/base_class/source_template_config.py
@@ -31,20 +31,18 @@ from gcp_airflow_foundations.base_class.column_udf_config import ColumnUDFConfig
 @dataclass
 class SourceTemplateConfig(SourceBaseConfig):
     """
-    Template configuration data class.w
+    Template configuration data class.
 
     Attributes:
-        table_names : List of table name. Used for Dag Id/s.
-        dest_table_override: Optional target table name. If None, use table_name instead.
         surrogate_keys : Keys used to identify unique records when merging into ODS.
         column_mapping : Mapping used to rename columns.
         cluster_fields: The fields used for clustering. BigQuery supports clustering for both partitioned and non-partitioned tables.
         landing_zone_table_name_override_template: Optional staging zone table name template.
+        dest_table_override_template: Optional target table name template. If None, uses table_name instead.
         column_casting : Mapping used to cast columns into a specific data type. Note column name uses that of the landing zone table.
         ods_config : ODS table configuration. See :class:`gcp_airflow_foundations.base_class.ods_table_config.OdsTableConfig`.
         hds_config : HDS table confidwguration. See :class:`gcp_airflow_foundations.base_class.hds_table_config.HdsTableConfig`.
         template_ingestion_options: Configuration for template-level ingestion.
-        facebook_table_config: Extra options for ingesting data from the Facebook API.
         extra_options: Field for storing additional configuration options.
         start_date : Start date override for DAG
         start_date_tz : Timezone
