541c08442976ea2eb6d32b96c13156622816f2b3
jean legrais <jeanlegrais@jeans-MacBook-Pro.local>
2022-08-09 09:15:44 -0400

    rmv bqtransfert

--- tests/integration/ods/test_ods_upsert.py
+++ tests/integration/ods/test_ods_upsert.py
@@ -2,7 +2,7 @@ import pytz
 import unittest
 from airflow.models import DAG, TaskInstance, XCom, DagRun, DagTag, DagModel
 from airflow.operators.dummy import DummyOperator
-from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
+# from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 from airflow.utils.session import create_session, provide_session
 from airflow.utils.state import State
@@ -145,7 +145,7 @@ class TestFullUpsertODS(unittest.TestCase):
         cleanup_xcom()
         clear_db_dags()
 
-        BigQueryToBigQueryOperator(
+        BigQueryHook().run_copy(
             source_project_dataset_tables="airflow-framework.test_tables.ga_sessions_ODS",
             destination_project_dataset_table=f"{PROJECT_ID}.{DATASET}.{self.table_id}",
             write_disposition="WRITE_TRUNCATE",
