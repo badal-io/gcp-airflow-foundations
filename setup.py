f14ea14d65a7c80f190eef729cbbf52eded022cf
jean legrais <jean.legrais@badal.io>
2022-10-14 12:45:31 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,63 @@
 import os
 import sys
-
+import yaml
 import setuptools
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Distribution
 
-from pathlib import Path
 
 here = os.path.abspath(os.path.dirname(__file__))
 about = {}
-
-with open(os.path.join(here, "requirements.txt"), "r") as f:
-    requirements = f.read().strip().split("\n")
+# Load extra dependencies from txt
 with open(os.path.join(here, "requirements-providers.txt"), "r") as f:
     requirements_providers = f.read().strip().split("\n")
 with open(os.path.join(here, "requirements-ci.txt"), "r") as f:
     requirements_test = f.read().strip().split("\n")
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
 
-packages = [
-    package
-    for package in setuptools.PEP420PackageFinder.find()
-    if package.startswith("gcp_airflow_foundations")
-]
+def main() -> dict:
+    if os.environ.get("PKG_NAME"):
+        pkg = os.environ.get("PKG_NAME")
 
-version = {}
-with open(os.path.join(here, "gcp_airflow_foundations/version.py")) as fp:
-    exec(fp.read(), version)
-version = version["__version__"]
+        # Framework configuration
+        if pkg == "gcp_airflow_foundations":
+            plug = yaml.safe_load(open("gcp_airflow_foundations_config/framework.yaml"))
+            return _process_metadata(plug)
 
+        # Facebook plugin configuration
+        if pkg == "gcp_airflow_foundations_facebook":
+            plug = yaml.safe_load(open("gcp_airflow_foundations_config/plugins_facebook.yaml"))
+            return _process_metadata(plug)
 
-def main():
+        else:
+            return None
+    else:
+        return None
+
+def _process_metadata(plug) -> dict:
+    packages = [
+        package
+        for package in setuptools.PEP420PackageFinder.find()
+        if package.startswith(str(plug['package-startwith']))
+    ]
+    extras = {}
+    for x in plug['extras']:
+        with open(os.path.join(here, f"requirements-{x}.txt"), "r") as f:
+            extra = f.read().strip().split("\n")
+        extras.update({x:extra})
     metadata = dict(
-        name="gcp-airflow-foundations",
-        version=version,
-        description="Opinionated framework based on Airflow 2.0 for building pipelines to ingest data into a BigQuery data warehouse",
-        long_description=long_description,
+        name = plug['name'],
+        version = plug['versions'][0],
+        description = plug['description'],
+        long_description=plug['long-description'],
+        packages=packages,
+        install_requires=plug['dependencies'],
+        extras_require=extras,
+    )
+    base_metadata = dict(
         long_description_content_type="text/markdown",
-        url="https://github.com/badal-io/gcp-airflow-foundations",
         author="Badal.io",
         author_email="info@badal.io",
         license="Apache 2.0",
-        packages=packages,
-        install_requires=requirements,
-        extras_require={'providers': requirements_providers, 'test': requirements_test},
         classifiers=[
             "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
@@ -56,9 +69,11 @@ def main():
             "Programming Language :: Python :: 3.9",
         ],
     )
+    metadata.update(base_metadata)
+    return metadata
 
-    setup(**metadata)
 
 
 if __name__ == "__main__":
-    main()
+    metadata = main()
+    setup(**metadata)
